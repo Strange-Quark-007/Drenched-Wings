24a0e98153bbd9025033d264f410634bf64c16bd
Strange-Quark-007 <masteryasin@ymail.com>
2025-12-12 17:30:22 +0530

    port to 1.21.11

--- src/main/java/strangequark/drenchedwings/mixin/ItemCooldownsAccessor.java
+++ src/main/java/strangequark/drenchedwings/mixin/ItemCooldownsAccessor.java
@@ -1,6 +1,6 @@
 package strangequark.drenchedwings.mixin;
 
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.ItemCooldowns;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
@@ -10,7 +10,7 @@ import java.util.Map;
 @Mixin(ItemCooldowns.class)
 public interface ItemCooldownsAccessor {
     @Accessor("cooldowns")
-    Map<ResourceLocation, ?> getRawCooldowns();
+    Map<Identifier, ?> getRawCooldowns();
 
     @Accessor("tickCount")
     int getTickCount();
